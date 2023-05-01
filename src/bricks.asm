0d3905437e8bc16f6f9cfb83a444f7a3f92b72c1
Martin Bernardi (PC) <martin@mbernardi.com.ar>
2023-05-01 15:52:51 -0300

    Copy level data to RAM

--- src/bricks.asm
+++ src/bricks.asm
@@ -26,12 +26,17 @@ reset:
         out     IO_LCD_W_INSTR, A
         call    lcd_clr_graphics        ; Clear graphics
 
+        ld      BC, 128                 ; Copy 128 bytes of level data to (tiles)
+        ld      HL, lvl_2
+        ld      DE, tiles
+        ldir
 ; Init ball
         ; ld      (ball_x), 64
         ; ld      (ball_y), 32
         ; ld      (ball_vx), 1
         ; ld      (ball_vy), 1
 _loop:
+
         call    draw_bricks
         halt
         jp      _loop
@@ -68,25 +73,30 @@ _loop:
 ;         jp      Z, _draw_ball_s_end
 
 
+; Args:
+; - Nothing
+; Ret:
+; - Nothing
+; Affects:
+; - A
+; - BC
+; - DE
+; - HL
 draw_bricks:
+        ld      D, tiles/255                ; DE holds tile memory address. Table is 256-aligned, so
+        ld      E, 0                        ; D will be high byte of table start address and the low
+                                            ; will be the offset or actual position of the
+                                            ; brick/tile. To get the Y position I can use E/16 and
+                                            ; the remainder is the X position
 
-        ; ld      (_tile_addr_h), tiles/255   ; Tile memory address. Table is 256-aligned, so the high
-        ; ld      (_cur_pos), 0               ; byte will be the table start and the low will be the
-        ;                                     ; offset or actual position of the brick/tile. To get
-        ;                                     ; the Y position I can use (_cur_pos)/16 and the
-        ;                                     ; remainder is the X position
-        ld      D, tiles/255
-        ld      E, 0
         ld      A, 0 | LCD_EI_GD_ADDR       ; Holds Y coordinate in pixels of the bottom of the tile
-                                            ; from the top of the screen
-        ld      (_cur_y), A
-
+        ld      (_cur_y), A                 ; from the top of the screen, I put it in another
+                                            ; variable so I dont have to divide E by 16 and to do
+                                            ; the OR with LCD_EI_GD_ADDR every time
 _draw_bricks_loop:
-
         ld      H, sprite_null/256          ; Get sprite table start (it starts at Y=3 of the
                                             ; sprite, and its 256-aligned)
 
-        ; ld      L, (_tile_addr)             ; Get tile to draw
         ld      A, (DE)                     ; Get tile to draw
 
         sla     A                           ; Multiply the ID by 4 to get the offset of address of
@@ -108,9 +118,8 @@ _draw_bricks_line_loop:
         call    lcd_wait
         outd                                ; Send to IO dev C, contents of (HL), decrement HL and B
         jr      NZ, _draw_bricks_line_loop
-
-; Increment addresses
-        inc     E
+; End of loop
+        inc     E                           ; Increment address
 
         ld      A, 0b00001111               ; See if E is multiple of 16 meaning we went down a line
         and     E
@@ -124,27 +133,24 @@ _draw_bricks_line_loop:
         ret
 
 
-
-; Levels in ROM. Each line of bricks is 16 bytes, and there are max 8 lines of 5 px tall
-
-                align   0x0100
-lvl_1:          db      1, 1, 1, 0, 0, 2, 2, 0, 0, 2, 2, 0, 0, 1, 1, 1
-                db      1, 1, 1, 0, 0, 2, 2, 0, 0, 2, 2, 0, 0, 1, 1, 1
-                db      1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1
-                db      1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1
-                db      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-                db      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-                db      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-                db      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-
-; Current bricks, its a modifyable copy of the level. Move to RAM
-
+; Levels in ROM. Each line of bricks is 16 bytes, and there are max 8 lines of 5 px tall.
+; I make aliases to TILE_AIR, TILE_BR0, etc so its easier
 __:             equ     TILE_AIR
 _0:             equ     TILE_BR0
 _1:             equ     TILE_BR1
-                align   0x0100
-tiles:          db      _0, _0, _0, __, __, _1, _1, __, __, _1, _1, __, __, _0, _0, _0
+_2:             equ     TILE_BR2
+
+lvl_1:          db      _0, _0, _0, __, __, _1, _1, __, __, _1, _1, __, __, _0, _0, _0
                 db      _0, _0, _0, __, __, _1, _1, __, __, _1, _1, __, __, _0, _0, _0
+                db      _0, _0, __, __, __, __, __, __, __, __, __, __, __, __, _0, _0
+                db      _0, _0, __, __, __, __, __, __, __, __, __, __, __, __, _0, _0
+                db      __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __
+                db      __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __
+                db      __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __
+                db      __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __
+
+lvl_2:          db      _2, _2, _0, _0, _0, _2, _2, _0, _0, _2, _2, _0, _0, _0, _2, _2
+                db      _2, _2, _0, _0, _0, _1, _1, _0, _0, _1, _1, _0, _0, _0, _2, _2
                 db      _0, _0, __, __, __, __, __, __, __, __, __, __, __, __, _0, _0
                 db      _0, _0, __, __, __, __, __, __, __, __, __, __, __, __, _0, _0
                 db      __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __
@@ -180,10 +186,10 @@ TILE_BR0:       equ     2
 sprite_br1:     db      0b01101100
 TILE_BR1:       equ     3
 
-                db      0b01111110
-                db      0b11111111
-                db      0b11111111
-sprite_br2:     db      0b01111110
+                db      0b01111100
+                db      0b11111110
+                db      0b11111110
+sprite_br2:     db      0b01111100
 TILE_BR2:       equ     4
 
                 db      0b01100000
@@ -197,6 +203,8 @@ sprite_ball:    db      0b01100000
 
 lives:          data    1
 level:          data    1
+                align   0x0100
+tiles:          data    16*8            ; Tiles, or state of the bricks of the level
 ; Positon in pixels
 ball_xy:                                ; ld BC,(ball_xy) will do B<-y, C<-x
 ball_x:         data    1
@@ -209,9 +217,4 @@ ball_vy:        data    1
 ; Length in 8px tiles
 pad_len:        data    1
 ; Variables for draw_bricks
-_tile_addr:                             ; table is 256-aligned, so _cur_pos will be the offset and I
-_cur_pos:         data    1             ; put it directly in the low byte
-_tile_addr_h:     data    1
-_cur_xy:                                ; ld BC,(_cur_xy) will do B<-y, C<-x
-_cur_x:         data    1
 _cur_y:         data    1
