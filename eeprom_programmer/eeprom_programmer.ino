f44aaa6fd7864ee3a7aca13eba4556c6a80f55c2
Martin Bernardi (PC) <martin@mbernardi.com.ar>
2024-02-05 21:14:45 -0300

    Fixed EEPROM programmer

--- eeprom_programmer/eeprom_programmer.ino
+++ eeprom_programmer/eeprom_programmer.ino
@@ -23,16 +23,46 @@
 
 #include "incbin.h"
 
-#define SHIFT_DATA 2
-#define SHIFT_CLK 3
-#define SHIFT_LATCH 4
-#define EEPROM_D0 5
-#define EEPROM_D7 12
-#define WRITE_EN 13
+#define OUTPUT_EN A0
+#define WRITE_EN 10
+#define DISABLED 9 // Tied to VCC in circuit
+#define SHIFT_DATA 6 // SER
+#define SHIFT_LATCH 7 // RCLK
+#define SHIFT_CLK 8 // SRCLK
+
+const byte EEPROM_DATA[] = { // Pins connected to D0, D1, D2, etc.
+  2,
+  3,
+  4,
+  A5,
+  A4,
+  A3,
+  A2,
+  A1,
+};
+
+const int ADDR_ORDER[] = { // Order in which address bits should enter the shift register
+  1 << 3,  // QH -> A3
+  1 << 2,  // QG -> A2
+  1 << 1,  // QF -> A1
+  1 << 0,  // QE -> A0
+  1 << 9,  // QD -> A9
+  1 << 11, // QC -> A11
+  1 << 10, // QB -> A10
+  0,       // QA -> Not connected 
+
+  1 << 14, // QH -> A14 (normally not available in EEPROM)
+  1 << 8,  // QG -> A8
+  1 << 13, // QF -> A13 (if jumper is connected)
+  1 << 12, // QE -> A12
+  1 << 7,  // QD -> A7
+  1 << 6,  // QC -> A6
+  1 << 5,  // QB -> A5
+  1 << 4,  // QA -> A4
+};
 
 //#define EEPROM_SIZE 8192
-#define EEPROM_SIZE 4098
-
+#define EEPROM_SIZE 4096
 
 // Change name of program to write:
 
@@ -46,9 +76,13 @@ INCBIN(Bin, "/home/mbernardi/documents/repos/other/z80mgc/out/main.bin");
 /*
  * Output the address bits and outputEnable signal using shift registers.
  */
-void setAddress(int address, bool outputEnable) {
-  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, (address >> 8) | (outputEnable ? 0x00 : 0x80));
-  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, address);
+void setAddress(int address) {
+
+  for (int i = 0; i < 16; i++) {
+    digitalWrite(SHIFT_DATA, (address & ADDR_ORDER[i]) != 0);
+    digitalWrite(SHIFT_CLK, HIGH);
+    digitalWrite(SHIFT_CLK, LOW);
+  }
 
   digitalWrite(SHIFT_LATCH, LOW);
   digitalWrite(SHIFT_LATCH, HIGH);
@@ -60,14 +94,15 @@ void setAddress(int address, bool outputEnable) {
  * Read a byte from the EEPROM at the specified address.
  */
 byte readEEPROM(int address) {
-  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
-    pinMode(pin, INPUT);
+  for (int i = 0; i < 8; i++) {
+    pinMode(EEPROM_DATA[i], INPUT);
   }
-  setAddress(address, /*outputEnable*/ true);
+  setAddress(address);
+  digitalWrite(OUTPUT_EN, LOW); // Enable out
 
   byte data = 0;
-  for (int pin = EEPROM_D7; pin >= EEPROM_D0; pin -= 1) {
-    data = (data << 1) + digitalRead(pin);
+  for (int i = 7; i >= 0; i--) {
+    data = (data << 1) + digitalRead(EEPROM_DATA[i]);
   }
   return data;
 }
@@ -77,15 +112,17 @@ byte readEEPROM(int address) {
  * Write a byte to the EEPROM at the specified address.
  */
 void writeEEPROM(int address, byte data) {
-  setAddress(address, /*outputEnable*/ false);
-  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
-    pinMode(pin, OUTPUT);
+  setAddress(address);
+  digitalWrite(OUTPUT_EN, HIGH); // Disable out
+  for (int i = 0; i < 8; i++) {
+    pinMode(EEPROM_DATA[i], OUTPUT);
   }
 
-  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
-    digitalWrite(pin, data & 1);
-    data = data >> 1;
+  for (int i = 0; i < 8; i++) {
+     digitalWrite(EEPROM_DATA[i], data & 1);
+     data = data >> 1;
   }
+
   digitalWrite(WRITE_EN, LOW);
   delayMicroseconds(1);
   digitalWrite(WRITE_EN, HIGH);
@@ -113,46 +150,18 @@ void printContents() {
 }
 
 
-/*
- * Read the contents of the EEPROM and print them to the serial monitor.
- */
-void printArray() {
-
-  PGM_P p = reinterpret_cast<PGM_P>(gBinData);
-
-  for (int base = 0; base <= gBinSize; base += 16) {
-    byte data[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    for (int offset = 0; offset <= 15 && base + offset < gBinSize; offset += 1) {
-      data[offset] = pgm_read_byte(p++);
-    }
-
-    char buf[80];
-    sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
-            base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-            data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
-
-    Serial.println(buf);
-  }
-}
-
-void setupa() {
-  pinMode(SHIFT_DATA, OUTPUT);
-  pinMode(SHIFT_CLK, OUTPUT);
-  pinMode(SHIFT_LATCH, OUTPUT);
-  digitalWrite(WRITE_EN, HIGH);
-  pinMode(WRITE_EN, OUTPUT);
-  Serial.begin(57600);
-  printArray();
-
-}
-
-
 void setup() {
   pinMode(SHIFT_DATA, OUTPUT);
   pinMode(SHIFT_CLK, OUTPUT);
   pinMode(SHIFT_LATCH, OUTPUT);
+  pinMode(DISABLED, INPUT);
+  for (int i = 0; i < 8; i++) {
+    pinMode(EEPROM_DATA[i], INPUT);
+  }
   digitalWrite(WRITE_EN, HIGH);
   pinMode(WRITE_EN, OUTPUT);
+  digitalWrite(OUTPUT_EN, HIGH);
+  pinMode(OUTPUT_EN, OUTPUT);
   Serial.begin(57600);
 
   // Erase entire EEPROM
